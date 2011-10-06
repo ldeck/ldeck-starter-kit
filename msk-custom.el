ea313ebf42bbcf7c8803cbbc0581c2b354eaeb60
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-06 14:54:13 +1100

    adjust ecb default highlight face and tree-guide-line face

--- msk-custom.el
+++ msk-custom.el
@@ -18,4 +18,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ecb-default-highlight-face ((t (:background "color-238" :foreground "color-35" :weight bold)))))
+ '(ecb-default-highlight-face ((t (:background "color-238" :foreground "color-106" :weight bold))))
+ '(ecb-tree-guide-line-face ((t (:inherit ecb-default-general-face :foreground "color-240" :height 1.0)))))
