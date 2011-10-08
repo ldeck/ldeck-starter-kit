4c551fcc71ecbfdbc97758162ac92a9fb59890c4
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-09 06:47:34 +1100

    remove unnessesaries

--- settings/msk-auto-complete-settings.el
+++ settings/msk-auto-complete-settings.el
@@ -1,6 +1,5 @@
 (provide 'msk-auto-complete-settings)
 
-(add-to-list 'load-path (file-expand-wildcards "~/.emacs.d/elpa/auto-complete-*/"))
 (require 'auto-complete)
 
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
--- settings/msk-ecb-settings.el
+++ settings/msk-ecb-settings.el
@@ -6,10 +6,6 @@
 (semantic-mode t)
 (global-ede-mode t)
 
-;(add-to-list 'load-path "~/.emacs.d/el-get/ecb")
-;(require 'ecb)
-;(ecb-activate)
-
 ;; some sensible key-strokes
 (global-set-key (kbd "M-1") 'ecb-goto-window-directories)
 (global-set-key (kbd "M-2") 'ecb-goto-window-sources)
