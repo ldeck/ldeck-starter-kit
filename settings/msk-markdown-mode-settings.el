332c7b0f27541bc8446f09de17f477cd533f3e64
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-10 10:01:08 +1100

    code comments

--- settings/msk-markdown-mode-settings.el
+++ settings/msk-markdown-mode-settings.el
@@ -9,4 +9,5 @@
 (add-to-list 'auto-mode-alist '("\\.mdt" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
 
+;; requires sudo port install multimarkdown
 (setq markdown-command "multimarkdown")
--- settings/msk-theme-settings.el
+++ settings/msk-theme-settings.el
@@ -1,8 +1,6 @@
-;; theme to use
+;; theme customisations
 (provide 'msk-theme-settings)
 
 ;(require 'color-theme)
 
-;; assume zenburn has been activated via el-get
-;; here we provide further customisations
 
