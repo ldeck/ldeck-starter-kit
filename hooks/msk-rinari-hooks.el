ffc3927d8e282a7c45058600a83d6f5455ea8fab
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-10 00:34:49 +1100

    rinari is now autoloaded by the msk-rinari-autoloads lib.

--- hooks/msk-rinari-hooks.el
+++ hooks/msk-rinari-hooks.el
@@ -1,15 +1,2 @@
 (provide 'msk-rinari-hooks)
 
-;; (defun msk-rinari-toggle-hook (dirold dirnew)
-;;   "hook to toggle rinari-minor-mode for ecb-after-directory-change-hook"
-;;     (cond
-;;      ((msk-rinari-active-p)
-;;       (progn
-;;         (print "deactivating rinari-minor-mode")
-;;         (rinari-minor-mode -1)))
-;;      (t
-;;       (progn
-;;         (print "activating rinari-minor-mode")
-;;         (rinari-minor-mode t)))))
-
-;(add-hook 'msk-ecb-after-directory-change-hook 'msk-rinari-toggle-hook)
