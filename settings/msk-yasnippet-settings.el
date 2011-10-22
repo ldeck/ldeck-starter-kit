402be21eb1e98053bed6503b76587125a90d53b6
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-22 21:05:29 +1100

    adding snippets dir as the preferred dir for custom yasnippets

--- settings/msk-yasnippet-settings.el
+++ settings/msk-yasnippet-settings.el
@@ -1,9 +1,19 @@
 (provide 'msk-yasnippet-settings)
 
-(if (file-exists-p (concat msk-user-dir "snippets"))
-    (progn
-      (setq yas/snippet-dirs
+(setq msk-yas-snippets-dir (concat msk-user-dir "snippets"))
+
+(unless (file-exists-p msk-yas-snippets-dir)
+  (make-directory msk-yas-snippets-dir))
+
+(setq yas/snippet-dirs
 	    (append
-	     (list (concat msk-user-dir "snippets"))
+	     (list msk-yas-snippets-dir)
              yas/snippet-dirs))
-      (yas/reload-all)))
+
+(yas/reload-all)
+
+;; When entering rinari-minor-mode, consider also the snippets in the
+;; snippet table "rails-mode"
+(add-hook 'rinari-minor-mode-hook
+          #'(lambda ()
+              (setq yas/mode-symbol 'rails-mode)))
